206e8bd2934f0c1b70b774d4e63ebbe337567426
nabielsubhan <mnsubhan1509@gmail.com>
2023-09-12 17:03:52 +0700

    Proyek Inventory Manager Tugas 2 PBP

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'appname': 'inventory-manager',
+        'name': 'Muhammad Nabiel Subhan',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
