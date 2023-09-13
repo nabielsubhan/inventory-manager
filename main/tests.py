56f73709be335ba5add0ed7088daa5c7765909a3
nabielsubhan <mnsubhan1509@gmail.com>
2023-09-13 10:19:56 +0700

    update tests.py

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item
 
 # Create your tests here.
 class mainTest(TestCase):
@@ -9,3 +10,14 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_create_template(self):
+        item = Item.objects.create(
+            name="an item",
+            amount="10",
+            description="the total of this item is 10"
+        )
+
+        self.assertEqual(item.name, "an item")
+        self.assertEqual(item.amount, "10")
+        self.assertEqual(item.description, "the total of this item is 10")
