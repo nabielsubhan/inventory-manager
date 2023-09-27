ca95dbc25bca28c209f0feb2f86e16bda0661825
nabielsubhan <mnsubhan1509@gmail.com>
2023-09-27 10:11:00 +0700

    Tugas 4 Done

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, \
+                        add_item, reduce_item, delete_item
 
 app_name = 'main'
 
@@ -9,5 +10,11 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id')
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('add-item/<int:id>/', add_item, name='add_item'),
+    path('reduce-item/<int:id>/', reduce_item, name='reduce_item'),
+    path('delete-item/<int:id>/', delete_item, name='delete_item'),
 ]
