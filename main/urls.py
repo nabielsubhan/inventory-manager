dceb04c9ed88486058d76668be82eed2490a83ba
nabielsubhan <mnsubhan1509@gmail.com>
2023-10-04 10:32:19 +0700

    Tugas 5 Done

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, \
-                        add_item, reduce_item, delete_item
+                        add_item, reduce_item, delete_item, edit_item
 
 app_name = 'main'
 
@@ -17,4 +17,5 @@ urlpatterns = [
     path('add-item/<int:id>/', add_item, name='add_item'),
     path('reduce-item/<int:id>/', reduce_item, name='reduce_item'),
     path('delete-item/<int:id>/', delete_item, name='delete_item'),
+    path('edit-item/<int:id>', edit_item, name='edit_item'),
 ]
